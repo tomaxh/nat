031c41279172697fa9f6f3c190eb2746a0f46b10
Tom Qin <tom.qin@leg.bc.ca>
2018-10-11 13:25:48 -0700

    tothetop

--- assets/nat/names_and_terms.js
+++ assets/nat/names_and_terms.js
@@ -912,6 +912,22 @@ function userAuth(){
 	});
 }
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 userAuth();
 
 $('#home').click(function(){location.reload()})
