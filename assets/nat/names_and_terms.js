b293725003608c76d422037701ea743f1efc473e
Tom Qin <tom.qin@leg.bc.ca>
2018-10-15 17:08:13 -0700

    d

--- assets/nat/names_and_terms.js
+++ assets/nat/names_and_terms.js
@@ -703,8 +703,13 @@ function update(){
 	$('.modal-3 #row2').append($('<div>').html(selectItem.verified));
 	$('.modal-3 #row2').append($('<div>').attr({"id":"titleinput"}));
 	var quillTitle = new Quill(".modal-3 #titleinput",reqOptions);
-	quillTitle.setContents(selectItem.verified)
+	quillTitle.setContents({
+		"ops":[
+			{"insert":selectItem.verified}
+		]
+	})
 	
+
 	$('.modal-3 #row2').append($('<label>').html("Verified Alternates: ").addClass("input-labels"));
 	$('.modal-3 #row2').append($('<div>').html(selectItem.verified_alternates));
 	$('.modal-3 #row2').append($('<div>').attr({"id":"titleinput2"}));
