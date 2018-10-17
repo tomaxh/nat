3c9fbd7a8e63a53ae1a7293f340b2943b7f47534
Tom Qin <tom.qin@leg.bc.ca>
2018-10-17 14:14:51 -0700

    s

--- assets/nat/names_and_terms.js
+++ assets/nat/names_and_terms.js
@@ -74,7 +74,10 @@ function buildResults(results) {
 	$.each(results, function(i, one) {
 		var item = $('<div>').addClass('item');
 		var title = $('<div>').addClass('title');
-		var popUp = $('<a>').addClass('popup').attr({"href":"#", "data-toggle":"popover", "data-trigger":"focus", "data-content":"Content Copied"})
+		var popUp = $('<div>').addClass('popup');
+		popUp.append$('<a>')
+						.addClass('popup')
+						.attr({"href":"#", "data-toggle":"popover", "data-trigger":"focus", "data-content":"Content Copied"})
 		title.append($('<div>')
 						.addClass('main-title')
 						.html(one.verified)
