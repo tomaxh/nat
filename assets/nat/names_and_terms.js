8501584459607ad1a511afd7d5c0583f036b425e
Tom Qin <tom.qin@leg.bc.ca>
2018-11-07 13:44:30 -0800

    whats new update

--- assets/nat/names_and_terms.js
+++ assets/nat/names_and_terms.js
@@ -1059,7 +1059,7 @@ function topFunction() {
 
 function logging(){
 	window.alert(
-		"1. 'Top' button now selects the textarea automatically. \n2. Stem toggle is now avaliable.\n3. Synonym search beta is now avaliable, try 'BC' or 'CIBC'\n4. Stop words won't be ignored anymore."
+		"1. 'Top' button now selects the textarea automatically. \n2. Stem toggle is now avaliable.\n3. Synonym search beta is now avaliable, try 'BC' or 'CIBC'\n4. Stop words won't be ignored anymore.\n5. Freezing caused by long-running script should be gone."
 	);
 }
 
